95c591b8276058a284b2bb5edf722ce2b1f8ca5f
areyouahuman <areyouahuman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-17 22:47:06 +0000

    Adding first version of our plugin, yay
    git-svn-id: https://plugins.svn.wordpress.org/are-you-a-human/trunk@464391 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ayah.php
@@ -0,0 +1,152 @@
+<?php
+/*
+ * Are You A Human
+ * PHP Integration Library
+ * 
+ * @version 0.0.9
+ * 
+ *    - Documentation and latest version
+ *          http://portal.areyouahuman.com/help
+ *    - Get an AYAH Publisher Key
+ *          https://portal.areyouahuman.com
+ *    - Discussion group
+ *          http://support.areyouahuman.com
+ *
+ * Copyright (c) 2011 AYAH LLC -- http://www.areyouahuman.com
+ *
+ * BY USING THIS SOFTWARE YOU AGREE TO THE TERMS AND CONDITIONS
+ * FOUND AT http://portal.areyouahuman.com/termsAndCondition
+ */
+
+class AYAH {
+	protected $publisher_key;
+	protected $scoring_key;
+	protected $webservice_host;
+	protected $timeout;
+	protected $session_secret;
+	
+	/**
+	 * Returns the markup for the PlayThru
+	 *
+	 * @return string
+	 */
+	public function getPublisherHTML() {
+		
+		$url = 'https://' . $this->webservice_host . "/ws/getIframe/";	
+		$response = $this->doCall($url, array('publisher_key' => $this->publisher_key), $this->timeout);
+
+		return $response->html;
+	}
+	
+	/**
+	 * Check whether the user is a human
+	 * Wrapper for the scoreGame API call
+	 *
+	 * @return boolean
+	 */
+	public function scoreResult() {
+		$result = false;
+		if ($this->session_secret) {
+			$url = 'https://' . $this->webservice_host . "/ws/scoreGame";
+			$fields = array(
+				'session_secret' => urlencode($this->session_secret),
+				'scoring_key' => $this->scoring_key
+			);
+			$resp = $this->doCall($url, $fields, false);
+
+			if ($resp) {
+				$result = ($resp->status_code == 1);
+			}
+			
+			if($resp->status_code != 1){
+				error_log('$resp: '.print_r($resp,true));
+			}
+		}
+
+		return $result;
+	}
+	
+	/**
+	 * Records a conversion
+	 * Called on the goal page that A and B redirect to
+	 * A/B Testing Specific Function
+	 *
+	 * @return boolean
+	 */
+	public function recordConversion(){
+		if( isset( $this->session_secret ) ){
+			return '<iframe style="border: none;" height="0" width="0" src="https://' . 
+				$this->ayah_web_service_host . '/ws/recordConversion/'.
+				$this->session_secret . '"></iframe>';
+		} else {
+			error_log('AYAH::recordConversion - AYAH Conversion Error: No Session Secret');
+			return FALSE;
+		}
+	}
+
+
+	public function __construct($params = array()) {
+		
+		//Get the sessionsecret
+		if(array_key_exists("session_secret", $_REQUEST)){
+			$this->session_secret = $_REQUEST["session_secret"];
+		}
+		
+		$config = parse_ini_file('ayah.config.php');
+		
+//		echo '<pre>';
+//		echo '$config: '.print_r($config,true).'<br>';
+//		echo '$params: '.print_r($params,true).'<br>';
+		
+		foreach($config as $key => $value){
+			if(array_key_exists($key, $params)){
+				$this->{$key} = $params[$key];
+			} else {
+				$this->{$key} = $value;
+			}
+		}
+		
+//		echo print_r($this,true);
+		
+		//exit();
+	}
+
+	// Internal:
+	// Makes a call with CURL
+	protected function doCall($url, $fields, $use_timeout) {
+		$fields_string = "";
+		foreach($fields as $key=>$value) { 
+			if (is_array($value)) {
+				foreach ($value as $v) {
+					$fields_string .= $key . '[]=' . $v . '&';
+				}
+			} else {
+				$fields_string .= $key.'='.$value.'&'; 
+			}
+		}
+		rtrim($fields_string,'&');
+		$curlsession = curl_init();		
+		curl_setopt($curlsession,CURLOPT_URL,$url);
+		curl_setopt($curlsession,CURLOPT_POST,count($fields));
+		curl_setopt($curlsession,CURLOPT_POSTFIELDS,$fields_string);
+		curl_setopt($curlsession,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($curlsession,CURLOPT_SSL_VERIFYHOST,0);
+		curl_setopt($curlsession,CURLOPT_SSL_VERIFYPEER,false);
+		if ($use_timeout) {
+			curl_setopt($curlsession,CURLOPT_TIMEOUT, $this->timeout);
+		}
+		$result = curl_exec($curlsession);		if ($result) {
+			try {
+				$m = json_decode( $result);
+			} catch (Exception $e) { 
+				error_log("AYAH::doCall() - Exception when calling json_decode: " . $e->getMessage());
+				$m = null;
+			}
+		} else {
+error_log(curl_error($curlsession));
+			$m = null;
+		}
+		curl_close($curlsession);	
+		return $m;
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,62 @@
+=== Plugin Name ===
+Contributors: areyouahuman
+Tags: anti-spam, spam, bot, comments, human, robot, areyouahuman, recaptcha
+Donate Link: http://portal.areyouahuman.com
+Requires at least: 3.2.1
+Tested up to: 3.2
+Stable tag: 0.1
+
+Captcha's suck. Are You A Human is trying to made a captcha that your visitors might actually enjoy using. Try out our beta!
+
+== Description ==
+
+This is the Beta release of the Are You A Human wordpress plugin.
+
+We are actively developing both the plugin and the PlayThru itself.
+
+It might not work everywhere, all the time, but we think it's good enough to start letting people test-drive it.
+
+Our PlayThru solves two problems: Humans hate captchas and bots are good at breaking them.
+
+We collect lots of data about how a visitor plays the game, which gives us many more dimensions to feed into our algorithm. We think that will make it more difficult for spammers. At the same time, we're trying to make games that people might actually enjoy playing.
+
+== Installation ==
+
+1. Upload `areyouahuman/` to the `/wp-content/plugins/` directory
+1. Register for an account at http://portal.areyouahuman.com
+1. Register your site under the Domains tab to get your API keys
+1. Insert your API keys in the admin panel under settings > areyouahuman
+
+== Frequently Asked Questions ==
+
+= What browsers do you support? =
+
+HTML5 Canvas Version
+
+* IE >= 9
+* Chrome >= 12.0
+* Firefox >= 5.0
+* Safari >= 4.0
+
+Adobe Flash Player (11 or Higher) Version
+
+* IE >= 7
+* Chrome >= 12.0
+* Firefox >= 5.0
+* Safari >= 3.2
+
+== Screenshots ==
+
+1. Admin panel
+1. Start Screen
+1. Game Play
+
+== Changelog ==
+
+= 0.1 =
+* Initial Beta release.
+
+== Upgrade Notice ==
+
+= 0.1 =
+* Initial Beta release.
